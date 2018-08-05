8c04fcf53ece6c682f5ba10fa8022dd2c2b1c4ce
Valmor F. de Almeida <dealmeidavf@gmail.com>
2018-08-05 14:48:49 -0400

    Reorg, editing.

--- src/application.py
+++ src/application.py
@@ -7,7 +7,6 @@ Cortix: a program for system-level modules coupling, execution, and analysis.
 Valmor F. de Almeida dealmeidav@ornl.gov; vfda
 Tue Dec 10 11:21:30 EDT 2013
 """
-
 #*********************************************************************************
 import os
 import sys
