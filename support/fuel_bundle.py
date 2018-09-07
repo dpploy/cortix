68b6c4ccf0df81bc861b657bafe0b9ccf979d480
gealas117 <gilberto_alas@student.uml.edu>
2018-09-07 15:41:17 -0400

    Added indentations Sphinx was complaining about

--- support/fuel_bundle.py
+++ support/fuel_bundle.py
@@ -11,9 +11,11 @@
 This FuelBundle class is a container for usage with other plant-level process modules.
 It is meant to represent a fuel bundle of an oxide fuel LWR reactor.
 There are three main data structures:
+
   1) fuel bundle specs
   2) solid phase
   3) gas phase
+
 The container user will have to provide all the data and from then on, this class
 will help acess the data.
 The printing methods reveal the contained data.
