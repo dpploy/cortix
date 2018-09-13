94db785f46e7235b30d39a0b252078248a908d17
Taha Azzaoui <tazzaoui@cs.uml.edu>
2018-09-12 22:11:53 -0400

    PyPI fixes

--- setup.py
+++ setup.py
@@ -3,22 +3,22 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
-
 setuptools.setup(
-    name="cortix8",
+    name="cortix",
     version="0.0.1",
-    author="Example Author",
-    author_email="author@example.com",
-    description="A small example package",
+    author="Valmor F. de Almeida",
+    author_email="Valmor_deAlmeida@uml.edu",
+    description=" Cortix is a Python library for system-level\
+                  module coupling, execution, and analysis.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
-    packages=['support', 'src', 'src.utils', 'tests'],
+    url="https://dpploy.github.io/cortix",
+    packages=['cortix', 'cortix.docs', 'cortix.examples',\
+              'cortix.modulib.pyplot',\
+              'cortix.src', 'cortix.support', 'cortix.tests'],
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
 )
