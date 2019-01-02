4e708a2d0154270ef677d29e6e3a791ede18ea97
Taha Azzaoui <taha@tahaazzaoui.com>
2019-01-02 17:58:41 -0500

    Added github banner to page

--- cortix/docs/conf.py
+++ cortix/docs/conf.py
@@ -126,7 +126,14 @@ html_theme = 'alabaster'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    'show_powered_by': False,
+    'github_user': 'dpploy',
+    'github_repo': 'cortix',
+    'github_banner': True,
+    'show_related': False,
+    'note_bg': '#FFF59C'
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
