7a1c87db79007e50ddabe5cc39f9f52ffcff71f2
Valmor F. de Almeida <dealmeidavf@gmail.com>
2019-03-17 17:09:12 -0400

    Update for cortix-nb

--- cortix/examples/notebook_run/droplet.py
+++ cortix/examples/notebook_run/droplet.py
@@ -19,8 +19,9 @@ from cortix import Cortix
 def run(task_name=None):
     '''
     Run the Cortix Droplet example in a Jupyter Notebook for the following tasks:
-     1. droplet-fall
-     2. solo-droplet-fall
+     1. solo-droplet
+     2. solo-pyplot
+     3. droplet-fall
 
     In a Jupyter Notebook cell enter:
    `    import cortix.examples.main.main_droplet as droplet
@@ -30,8 +31,8 @@ def run(task_name=None):
     full_path_config_file = os.path.join(pwd, '../input/cortix-config-droplet.xml')
     cortix = Cortix('cortix-droplet', full_path_config_file)
 
-    assert task_name == 'droplet-fall' or task_name == 'solo-droplet-fall',\
-           'FATAL: task name %r invalid.'%task_name
+    assert task_name == 'droplet-fall' or task_name == 'solo-droplet' or\
+            task_name == 'solo-pyplot','FATAL: task name %r invalid.'%task_name
 
     cortix.run_simulations(task_name=task_name)
 
