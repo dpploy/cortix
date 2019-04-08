644cd48055f3fa11b1126270e2488296cb90f8da
Austin Rotker <austin_rotker@student.uml.edu>
2019-04-08 12:29:46 -0400

    updated comments to actor, fuel_bucket, fuel_bundle and fuel_segment

--- cortix/support/fuel_bundle.py
+++ cortix/support/fuel_bundle.py
@@ -33,7 +33,10 @@ from copy import deepcopy
 # *******************************************************************************
 
 class FuelBundle():
-    '''Represents a bundle of cylindrical fuel rods bound together into a single unit. These fuel rods are composed of an outer wall of a certain thickness comprised of structural metal and an inner pin cell that contains fuel. Constructor requires three data structures to be passed to it, all as pandas: one for the fuel bundle specifications, one for information on the solid phase and one for information on the gas phase.'''
+    """
+    Represents a bundle of cylindrical fuel rods bound together into a single unit. These fuel rods are composed of an outer wall of a certain thickness comprised of structural metal and an inner pin cell that contains fuel. Constructor requires three data structures to be passed to it, all as pandas: one for the fuel bundle specifications, one for information on the solid phase and one for information on the gas phase.
+    
+    """
 
     def __init__(self,
                  specs=pandas.DataFrame()
@@ -53,36 +56,98 @@ class FuelBundle():
     # Start: Pre-irradiation information
 
     def get_name(self):
-        '''returns the name of the fuel bundle.'''
+        """
+        returns the name of the fuel bundle.
+                
+        Parameters
+        ----------
+        empty:
+        
+        Returns:
+        ----------
+        name: str
+        """
         return self.__specs.loc['Name', 1]
     name = property(get_name, None, None, None)
 
     def get_fuel_enrichment(self):
-        '''returns the enrichment of the fuel pins in the bundle in terms of atomic abundance of Uranium-235.'''
+        """
+        returns the enrichment of the fuel pins in the bundle in terms of atomic abundance of Uranium-235.
+                
+        Parameters
+        ----------
+        empty:
+        
+        Returns:
+        ----------
+        fuel_enrichment: float
+        """
+        
         return self.__get_fuel_enrichment()
     fuel_enrichment = property(get_fuel_enrichment, None, None, None)
 
     def get_fresh_u_mass(self):
-        '''returns the total mass of all uranium in the fuel bundle in units of grams.'''
+        """
+        returns the total mass of all uranium in the fuel bundle in units of grams.
+                
+        Parameters
+        ----------
+        empty:
+        
+        Returns:
+        ----------
+        fresh_u_mass: float
+        """
+        
         return self.__get_fresh_u_mass()
     fresh_u_mass = property(get_fresh_u_mass, None, None, None)
 
     def get_fresh_u238_mass(self):
-        '''returns the total mass of all Uranium-238 in the fuel bundle in units of grams.'''
+        """
+        returns the total mass of all Uranium-238 in the fuel bundle in units of grams.
+                
+        Parameters
+        ----------
+        empty:
+        
+        Returns:
+        ----------
+        fresh_u238_mass: float
+        """
         total_mass = self.__get_fresh_u_mass()
         fuel_enrichment = self.__get_fuel_enrichment()
         return total_mass * (1.0 - fuel_enrichment / 100.0)
     fresh_u238_mass = property(get_fresh_u238_mass, None, None, None)
 
     def get_fresh_U235_mass(self):
-        '''returns the total mass of all Uranium-235 in the fuel bundle in units of grams.'''
+        """
+        returns the total mass of all Uranium-235 in the fuel bundle in units of grams.
+                
+        Parameters
+        ----------
+        empty:
+        
+        Returns:
+        ----------
+        fresh_u235_mass: float
+        """
         total_u_Mass = self.__get_fresh_u_mass()
         fuel_enrichment = self.__get_fuel_enrichment()
         return total_u_mass * fuel_enrichment / 100.0
     fresh_u235_mass = property(get_fresh_U235_mass, None, None, None)
 
     def get_n_fuel_rods(self):
-        '''returns the number of fuel rods comprising the fuel bundle.'''
+        """
+        returns the number of fuel rods comprising the fuel bundle.
+                
+        Parameters
+        ----------
+        empty:
+        
+        Returns:
+        ----------
+        n_fuel_rods: float
+        """
         return self.__get_n_fuel_rods()
     n_fuel_rods = property(get_n_fuel_rods, None, None, None)
 
@@ -90,93 +155,274 @@ class FuelBundle():
     # ------
 
     def get_fuel_pin_length(self):
-        '''returns the length of a fuel rod in cm.'''
+        """
+        returns the length of a fuel rod in cm.
+                
+        Parameters
+        ----------
+        empty:
+        
+        Returns:
+        ----------
+        fuel_pin_length: float
+        """
         return self.__get_fuel_pin_length()
 
     def set_fuel_pin_length(self, x):
-        '''sets the length of each fuel rod to a new value. Used for chopping.'''
+        """
+        sets the length of each fuel rod to a new value. Used for chopping.
+                
+        Parameters
+        ----------
+        x: float
+        
+        Returns:
+        ----------
+        empty:
+        """
         self.__specs.loc['Fuel rods fuel length [in]', 1] = x / 2.54  # in
         return
     fuel_pin_length = property(get_fuel_pin_length, set_fuel_pin_length, None, None)
 
     def get_fuel_rod_od(self):
-        '''returns the outer diameter of a fuel rod in cm.'''
+        """
+        returns the outer diameter of a fuel rod in cm.
+                
+        Parameters
+        ----------
+        empty:
+        
+        Returns:
+        ----------
+        fuel_rod_od: float
+        """
         return self.__get_fuel_rod_od()
     fuel_rod_od = property(get_fuel_rod_od, None, None, None)
 
     def get_fuel_pin_radius(self):
-        '''returns the radius of an interior fuel pin.'''
+        """
+        returns the radius of an interior fuel pin.
+                
+        Parameters
+        ----------
+        empty:
+        
+        Returns:
+        ----------
+        fuel_pin_radius: float
+        """
         return self.__get_fuel_pin_radius()
     fuel_pin_radius = property(get_fuel_pin_radius, None, None, None)
 
     def get_fuel_pin_volume(self):
-        '''returns the total volume of fuel in a fuel pinm (and by extension the total volume of fuel in a fuel rod).'''
+        """
+        returns the total volume of fuel in a fuel pinm (and by extension the total volume of fuel in a fuel rod).
+                
+        Parameters
+        ----------
+        empty:
+        
+        Returns:
+        ----------
+        fuel_pin_volume: float
+        """
         return self.__get_fuel_pin_volume()
     fuel_pin_volume = property(get_fuel_pin_volume, None, None, None)
 
     def get_fuel_volume(self):
-        '''returns the total volume of fuel in the entire bundle.'''
+        """
+        returns the total volume of fuel in the entire bundle.
+                
+        Parameters
+        ----------
+        empty:
+        
+        Returns:
+        ----------
+        fuel_volume: float
+        """
         return self.__get_fuel_volume()
     fuel_volume = property(get_fuel_volume, None, None, None)
     
     def get_fuel_mass(self):
-        '''returns the mass density of the solid fuel.'''
+        """
+        returns the mass density of the solid fuel.
+                
+        Parameters
+        ----------
+        empty:
+        
+        Returns:
+        ----------
+        fuel_mass: float
+        """
         return self.__solid_phase.GetValue('mass')
     fuel_mass = property(get_fuel_mass, None, None, None)
 
     def get_fuel_mass_unit(self):
-        '''returns the units used to measure mass density of the solid fuel.'''
+        """
+        returns the units used to measure mass density of the solid fuel.
+                
+        Parameters
+        ----------
+        empty:
+        
+        Returns:
+        ----------
+        fuel_mass_unit: str
+        """
         return self.__solid_phase.GetQuantity('mass').unit
     fuel_mass_unit = property(get_fuel_mass_unit, None, None, None)
 
     def get_gas_mass(self):
-        '''returns the mass density of fuel in the gas phase, in units of g/cc.'''
+        """
+        returns the mass density of fuel in the gas phase, in units of g/cc.
+                
+        Parameters
+        ----------
+        empty:
+        
+        Returns:
+        ----------
+       gas_mass: float
+       """
         return self.__gas_phase.GetValue('mass')
     gas_mass = property(get_gas_mass, None, None, None)
 
     def get_radioactivity(self):
-        '''returns the radioactivity density of the fuel, in units of Ci/cc.'''
+        """
+        returns the radioactivity density of the fuel, in units of Ci/cc.
+                
+        Parameters
+        ----------
+        empty:
+        
+        Returns:
+        ----------
+        radioactivity: float
+        """
         return self.__solid_phase.GetValue('radioactivity') + \
             self.__gas_phase.GetValue('radioactivity')
     radioactivity = property(get_radioactivity, None, None, None)
 
     def get_gamma_pwr(self):
-        '''returns density of gamma radiation emitted per unit time in the fuel, in units of watts/cc.'''
+        """
+        returns density of gamma radiation emitted per unit time in the fuel, in units of watts/cc.
+                
+        Parameters
+        ----------
+        empty:
+        
+        Returns:
+        ----------
+        gamma_pwr: float
+        """
         return self.__solid_phase.GetValue('gamma') + \
             self.__gas_phase.GetValue('gamma')
     gamma_pwr = property(get_gamma_pwr, None, None, None)
 
     def get_heat_pwr(self):
-        '''returns the density of heat energy generated by radioactive decay in the fuel, in units of watts/cc.'''
+        """
+        returns the density of heat energy generated by radioactive decay in the fuel, in units of watts/cc.
+                
+        Parameters
+        ----------
+        empty:
+        
+        Returns:
+        ----------
+        heat_pwr: float
+        """
         return self.__solid_phase.GetValue('heat') + \
             self.__gas_phase.GetValue('heat')
     heat_pwr = property(get_heat_pwr, None, None, None)
 
     def get_fuel_radioactivity(self):       
-        '''returns the radioactivity density of the fuel in the solid phase, in units of Ci/cc.'''
+        """
+        returns the radioactivity density of the fuel in the solid phase, in units of Ci/cc.
+                
+        Parameters
+        ----------
+        empty:
+        
+        Returns:
+        ----------
+        fuel_radioactivity: float
+        """
         return self.__solid_phase.GetValue('radioactivity')
     fuel_radioactivity = property(get_fuel_radioactivity, None, None, None)
 
     def get_gas_radioactivity(self):  
-        '''returns the radioactivity density of the fuel in the gas phase, in units of Ci/cc.'''
+        """
+        returns the radioactivity density of the fuel in the gas phase, in units of Ci/cc.
+                
+        Parameters
+        ----------
+        empty:
+        
+        Returns:
+        ----------
+        gas_radioactivity: float
+        """
         return self.__gas_phase.GetValue('radioactivity')
     gas_radioactivity = property(get_gas_radioactivity, None, None, None)
 
     def get_solid_phase(self):
-        '''returns solid phase history of the fuel.'''
+        """
+        returns solid phase history of the fuel.
+                
+        Parameters
+        ----------
+        empty:
+        
+        Returns:
+        ----------
+        solid_phase: DataFrame
+        """
         return self.__solid_phase
 
     def set_solid_phase(self, phase):
-        '''sets the current solid phase to specific values.'''
+        """sets the current solid phase to specific values.
+                
+        Parameters
+        ----------
+        phase: DataFrame
+        
+        Returns:
+        ----------
+        empty:
+        """
+        
         self.__solid_phase = deepcopy(phase)
     solid_phase = property(get_solid_phase, set_solid_phase, None, None)
 
     def get_gas_phase(self):
-        '''returns gas phase history of the fuel.'''
+        """
+        returns gas phase history of the fuel.
+                
+        Parameters
+        ----------
+        empty:
+        
+        Returns:
+        ----------
+        gas_phase: DataFrame
+        """
         return self.__gas_phase
 
     def set_gas_phase(self, phase):
-        '''sets the gas phase of the fuel to specific values.'''
+        """
+        sets the gas phase of the fuel to specific values.
+                
+        Parameters
+        ----------
+        phase: DataFrame
+        
+        Returns:
+        ----------
+        empty:
+        """
+        self.__get_slug_length(): float
         self.__gas_phase = deepcopy(phase)
     gas_phase = property(get_gas_phase, set_gas_phase, None, None)
 
