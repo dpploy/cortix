895752d6e92de74124d4cdec12948d6e74703286
Valmor F. de Almeida <dealmeidavf@gmail.com>
2019-05-26 21:47:54 -0400

    Wind added to pytest.

---
+++ cortix/tests/test_wind.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# This file is part of the Cortix toolkit environment
+# https://cortix.org
+#
+# All rights reserved, see COPYRIGHT for full restrictions.
+# https://github.com/dpploy/cortix/blob/master/COPYRIGHT.txt
+#
+# Licensed under the University of Massachusetts Lowell LICENSE:
+# https://github.com/dpploy/cortix/blob/master/LICENSE.txt
+
+import os
+from cortix import Cortix
+
+def test_wind():
+    '''
+    Run the wind example
+    '''
+    pwd = os.path.dirname(__file__)
+    full_path_config_file = os.path.join(pwd, 'input/cortix-config-wind.xml')
+    cortix1 = Cortix('cortix-wind', full_path_config_file)
+    cortix1.run_simulations(task_name="wind-dry")
+    os.system('rm -f *.png')
+    os.system('rm -f *.pickle')
+
+if __name__ == "__main__":
+    test_wind()
