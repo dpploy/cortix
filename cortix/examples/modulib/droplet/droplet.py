d0599dc8e4483563cc1e29f1957446928e2a3509
Valmor F. de Almeida <dealmeidavf@gmail.com>
2019-06-02 16:19:43 -0400

    Droplet with native 3-D vortex wind.

--- cortix/examples/modulib/droplet/droplet.py
+++ cortix/examples/modulib/droplet/droplet.py
@@ -120,9 +120,8 @@ class Droplet():
         self.__ode_params = dict() # hold parameters for the ODE solver
 
         import scipy.constants as const
-        # Create a drop with random diameter up within 5 and 8 mm
+        # Create a drop with random diameter up within 5 and 8 mm.
         diam = (npy.random.random(1)*(8 - 5) + 5)[0]
-        diam = 8 # hardcoded remove this later
         self.__droplet_diameter = diam * const.milli # [m]
 
         gravity = const.g # acceleration of gravity SI
@@ -131,8 +130,7 @@ class Droplet():
 
         # Drag coefficient must be npy.ndarray (see ODE solvers).
         drag_variation = (npy.random.random(1)*(1.0 - 0.8) + 0.8)[0]
-        drag_variation = 0.85 # hardcoded remove this later
-        self.__ode_params['drag-coeff']    = npy.ones(3)*drag_variation * 1e-4
+        self.__ode_params['drag-coeff'] = npy.ones(3)*drag_variation * 5.0e-4
 
         # Setup species in the liquid phase.
         species = list()
@@ -177,42 +175,59 @@ class Droplet():
         # z coordinate pointing upwards. -L <= x <= L, -L <= y <= L, 
         self.__box_half_length = 250.0 # [m]
         x_0 = ( 2*npy.random.random(3) - npy.ones(3) ) * self.__box_half_length
-        box_height = 100.0 # [m]
-        x_0[2] = box_height
+        self.__box_height = 100.0 # H [m]
+        x_0[2] = self.__box_height
         self.__liquid_phase.SetValue( 'position', x_0, self.__start_time )
 
         # Vortex model for initial velocity (this is for the sake of testing droplet
-        # motion with wind drag)
+        # motion with wind drag).
         # Wind velocity must be npy.ndarray (see ODE solvers).
         self.__min_core_radius = 5.0 # [m]
         self.__outer_v_theta = 1.0 # m/s # angular speed
-        self.__v_z = 0.50 # [m/s]
+        self.__v_z_0 = 0.50 # [m/s]
 
         wind_velocity = self.__vortex_velocity( x_0 )
 
-        self.__ode_params['wind-velocity'] = wind_velocity
-        #print(self.__ode_params['wind-velocity'])
+        # Save the function reference for future dispatch
+        self.__ode_params['wind-velocity-function'] = self.__vortex_velocity
 
+        # Set the initial velocity of the droplet to the wind velocity
         self.__liquid_phase.SetValue( 'velocity', wind_velocity, self.__start_time )
 
-        # For insight on the vortex flow field:
-        xaxis = list()
-        yaxis = list()
-        for dx in npy.linspace(0,1,500):
-            x = dx * self.__box_half_length
-            xaxis.append(x)
-            y = 0.0
-            z = box_height
-            wind_velocity = self.__vortex_velocity( npy.array([x,y,z]) )
-            yaxis.append(wind_velocity[1])
-            #print(wind_velocity)
+        # Plot for insight on the vortex flow field.
         import matplotlib.pyplot as plt
-        fig=plt.figure(1)
-        plt.plot(xaxis,yaxis)
+        fig = plt.figure(1)
+        plt.subplots_adjust(hspace=0.5)
+
+        for z in npy.linspace(0,self.__box_height,3):
+            xval = list()
+            yval = list()
+            for x in npy.linspace(0,self.__box_half_length,500):
+                xval.append(x)
+                y = 0.0
+                wind_velocity = self.__vortex_velocity( npy.array([x,y,z]) )
+                yval.append(wind_velocity[1])
+                #print(wind_velocity)
+            plt.subplot(2,1,1)
+            plt.plot(xval,yval)
+
         plt.xlabel('Radial distance [m]')
         plt.ylabel('Tangential speed [m/s]')
         plt.title('Vortex Wind')
         plt.grid()
+
+        xval = list()
+        yval = list()
+        for z in npy.linspace(0,self.__box_height,50):
+            yval.append(z)
+            wind_velocity = self.__vortex_velocity( npy.array([0.0,0.0,z]) )
+            xval.append(wind_velocity[2])
+        plt.subplot(2,1,2)
+        plt.plot(xval,yval)
+        plt.xlabel('Vertical speed [m]')
+        plt.ylabel('Height [m/s]')
+        plt.grid()
+
         fig.savefig('vortex-wind.png',dpi=200,format='png')
 
         return
@@ -627,10 +642,10 @@ class Droplet():
                 # Get this Droplet's velocity
                 velocity = wind_phase.GetValue(velocity_name,at_time)
 
-                print('')
-                print('DROPLET: DROPLET VELOCITY RECEIVED')
-                print(velocity)
-                print('')
+                #print('')
+                #print('DROPLET: DROPLET VELOCITY RECEIVED')
+                #print(velocity)
+                #print('')
 
                 #loc = velocity.value.index.get_loc(at_time,method='nearest',
                 #        tolerance=1e-2)
@@ -704,21 +719,27 @@ class Droplet():
 
             def rhs_fn(t, u_vec, dt_u_vec, params):
 
-                alpha     = params['drag-coeff']
-                wind_velo = params['wind-velocity']
+                drop_pos = u_vec[:3]
+                alpha    = params['drag-coeff']
+                try:
+                    wind_velo = params['wind-velocity'] # try port connected data
+                except:
+                    wind_velo_func = params['wind-velocity-function'] # use function
+                    wind_velo      = wind_velo_func(drop_pos)
+
                 drop_velo = u_vec[3:]
                 drag      = - alpha * (drop_velo - wind_velo)
                 gravity   = params['gravity']
                 mass      = params['droplet_mass']
 
                 dt_u_vec[0] = u_vec[3]                            #  d_t u_1 = u_4
-                dt_u_vec[3] = drag[0]/mass                        #  d_t u_4 = 0 
+                dt_u_vec[3] = drag[0]/mass                        #  d_t u_4 = f_1
 
                 dt_u_vec[1] = u_vec[4]                            #  d_t u_2 = u_5
-                dt_u_vec[4] = drag[1]/mass                        #  d_t u_5 = 0 
+                dt_u_vec[4] = drag[1]/mass                        #  d_t u_5 = f_2
 
                 dt_u_vec[2] = u_vec[5]                            #  d_t u_3 = u_6
-                dt_u_vec[5] = drag[2]/mass - gravity                  #  d_t u_6 = -g
+                dt_u_vec[5] = drag[2]/mass - gravity              #  d_t u_6 = f_3 -g
 
                 return
 
@@ -726,8 +747,14 @@ class Droplet():
 
             def rhs_fn(u_vec, t, params):
 
-                alpha     = params['drag-coeff']
-                wind_velo = params['wind-velocity']
+                drop_pos = u_vec[:3]
+                alpha    = params['drag-coeff']
+                try:
+                    wind_velo = params['wind-velocity'] # try port connected data
+                except:
+                    wind_velo_func = params['wind-velocity-function'] # use function
+                    wind_velo      = wind_velo_func(drop_pos)
+
                 drop_velo = u_vec[3:]
                 drag      = - alpha * (drop_velo - wind_velo)
                 gravity   = params['gravity']
@@ -812,7 +839,6 @@ class Droplet():
         box_half_length = self.__box_half_length
         min_core_radius = self.__min_core_radius
         outer_v_theta   = self.__outer_v_theta
-        v_z             = self.__v_z
 
         outer_cylindrical_radius = math.hypot(box_half_length,box_half_length)
         circulation = 2*math.pi * outer_cylindrical_radius * outer_v_theta # m^2/s
@@ -823,11 +849,16 @@ class Droplet():
         y = position[1]
         z = position[2]
 
+        relax_length = self.__box_height/5.0
+        z_relax_factor = math.exp(-(self.__box_height-z)/relax_length)
+        v_z = self.__v_z_0 * z_relax_factor
+
         cylindrical_radius = math.hypot(x,y)
         azimuth = math.atan2(y,x)
 
-        v_theta = (1 - math.exp(-cylindrical_radius**2/core_radius**2) ) * \
-                   circulation/2/math.pi/max(cylindrical_radius,min_core_radius)
+        v_theta = (1 - math.exp(-cylindrical_radius**2/core_radius**2) ) *\
+                   circulation/2/math.pi/max(cylindrical_radius,min_core_radius) *\
+                   z_relax_factor
 
         v_x = - v_theta * math.sin(azimuth)
         v_y =   v_theta * math.cos(azimuth)
