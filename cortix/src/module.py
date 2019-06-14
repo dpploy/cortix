78ed694b6dda4b97c3613f14e0ecb81e40b3af7b
Taha Azzaoui <taha@azzaoui.org>
2019-06-14 14:35:32 -0400

    added module skeleton

---
+++ cortix/src/module.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# This file is part of the Cortix toolkit environment
+# https://cortix.org
+
+class Module:
+    """
+    The representation of a Cortix module. This class is to be inherited by every
+    Cortix module. It provides facilities for creating and connecting modules within
+    the Cortix network.
+    """
+    def __init__(self):
+        # list of ports (must be populated by the module)
+        self.ports =  []
+
+    def send(self, data, port):
+        """
+        Send data through a given provide port
+        """
+        pass
+
+    def recv(self, port):
+        """
+        Receive data from a given use port
+        """
+        pass
