5c67508ba61b8846256a6dca99b778d01bf5a38a
Taha Azzaoui <taha@azzaoui.org>
2019-06-14 14:36:05 -0400

    Added unit test to main

--- cortix/src/cortix_main.py
+++ cortix/src/cortix_main.py
@@ -2,20 +2,14 @@
 # -*- coding: utf-8 -*-
 # This file is part of the Cortix toolkit environment
 # https://cortix.org
-#
-# All rights reserved, see COPYRIGHT for full restrictions.
-# https://github.com/dpploy/cortix/blob/master/COPYRIGHT.txt
-#
 # Licensed under the University of Massachusetts Lowell LICENSE:
 # https://github.com/dpploy/cortix/blob/master/LICENSE.txt
 
-#*********************************************************************************
 import os
 import shutil
 import logging
 from cortix.src.utils.cortix_units import Units
 from cortix.src.utils.cortix_time import CortixTime
-#*********************************************************************************
 
 class Cortix:
     '''
@@ -23,12 +17,12 @@ class Cortix:
     '''
 
     def __init__(self, name, work_dir="/tmp/"):
-        self.__name = name
-        self.__work_dir = work_dir + self.__name + '-wrk/'
+        self.name = name
+        self.work_dir = os.path.join(work_dir, self.name + '-wrk/')
 
         # Create the work directory
-        shutil.rmtree(self.__work_dir, ignore_errors=True)
-        os.mkdir(self.__work_dir)
+        shutil.rmtree(self.work_dir, ignore_errors=True)
+        os.mkdir(self.work_dir)
 
         # Setup the global logger
         self.__create_logger()
@@ -40,11 +34,7 @@ class Cortix:
 
         self.modules = list()
 
-        self.log.info('Created Cortix object %s', self.__name)
-
-#*********************************************************************************
-# Public member functions
-#*********************************************************************************
+        self.log.info('Created Cortix object %s', self.name)
 
     def add_module(self, m):
         assert isinstance(m, Module), "m must be a module"
@@ -58,24 +48,18 @@ class Cortix:
         pass
         return
 
-#*********************************************************************************
-# Private helper functions (internal use: __)
-#*********************************************************************************
-
     def __create_logger(self):
         '''
         A helper function to setup the logging facility used in constructor
         '''
-
         self.log = logging.getLogger("cortix")
         self.log.setLevel(logging.DEBUG)
 
-        file_handler = logging.FileHandler(self.__work_dir + 'cortix.log')
+        file_handler = logging.FileHandler(self.work_dir + 'cortix.log')
         file_handler.setLevel(logging.DEBUG)
 
         console_handler = logging.StreamHandler()
         console_handler.setLevel(logging.DEBUG)
-
 
         # Formatter added to handlers
         formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -85,7 +69,6 @@ class Cortix:
         # add handlers to logger
         self.log.addHandler(file_handler)
         self.log.addHandler(console_handler)
-#======================= end class Cortix: =======================================
 
 if __name__ == "__main__":
     c = Cortix("test")
