add440f555dd6d548c137873b09f0e5e6150500c
Valmor F. de Almeida <dealmeidavf@gmail.com>
2019-07-03 11:13:15 -0400

    Loose ends.

--- cortix/__init__.py
+++ cortix/__init__.py
@@ -1,11 +1,6 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # This file is part of the Cortix toolkit environment
-# https://github.com/dpploy/...
-#
-# All rights reserved, see COPYRIGHT for full restrictions.
-# https://github.com/dpploy/COPYRIGHT
-#
-# Licensed under the GNU General Public License v. 3, please see LICENSE file.
-# https://www.gnu.org/licenses/gpl-3.0.txt
-#
+# https://cortix.org
+
 from .src.cortix_main import Cortix
