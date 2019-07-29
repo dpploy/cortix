6370145314c30ca1eff2d6dd2cec9904ec18aade
Valmor F. de Almeida <dealmeidavf@gmail.com>
2019-07-29 19:27:23 -0400

    Reorg of run_droplet for users better understanding of the networks used

--- cortix/examples/run_droplet.py
+++ cortix/examples/run_droplet.py
@@ -58,76 +58,115 @@ command line as
 if __name__ == "__main__":
 
     # Configuration Parameters
-    use_single_plot = True # True for a single plot output
+    use_single_plot = True  # True for a single plot output
                             # False for multiple plot files and network
     use_mpi         = True
 
     n_droplets = 5
-    end_time   = 300
+    end_time   = 30
     time_step  = 0.1
 
     cortix = Cortix(use_mpi=use_mpi)
 
-    # Vortex module (single).
-    vortex = Vortex()
-    #cortix.add_module(vortex)
-    vortex.show_time = (True,100)
-    vortex.end_time = end_time
-    vortex.time_step = time_step
-    vortex.plot_velocity()
-
-    # DataPlot module (single).
+    # Network for a single plot case
     if use_single_plot:
+
+        # Vortex module (single).
+        vortex = Vortex()
+        #cortix.add_module(vortex)
+        vortex.show_time = (True,100)
+        vortex.end_time = end_time
+        vortex.time_step = time_step
+        vortex.plot_velocity()
+
+        # DataPlot module (single).
         data_plot = DataPlot()
         #cortix.add_module(data_plot)
         data_plot.title = 'Droplet Trajectories'
         data_plot.same_axes = True
         data_plot.dpi = 300
 
-    for i in range(n_droplets):
+        for i in range(n_droplets):
 
-        # Droplet modules.
-        droplet = Droplet()
-        #cortix.add_module(droplet)
-        droplet.end_time = end_time
-        droplet.time_step = time_step
-        droplet.bounce = False
-        droplet.slip = False
-        # Ports def.
-        external_flow = Port('external-flow')
-        droplet.add_port(external_flow)
-        visualization = Port('visualization')
-        droplet.add_port(visualization)
+            # Droplet modules.
+            droplet = Droplet()
+            #cortix.add_module(droplet)
+            droplet.end_time = end_time
+            droplet.time_step = time_step
+            droplet.bounce = False
+            droplet.slip = False
+            # Ports def.
+            external_flow = Port('external-flow')
+            droplet.add_port(external_flow)
+            visualization = Port('visualization')
+            droplet.add_port(visualization)
 
-        # DataPlot modules (multiple).
-        if not use_single_plot:
+            # DataPlot module ports def.
+            plot = Port('viz-data:{:05}'.format(i))
+            data_plot.add_port(plot)
+
+            # Vortex module ports def.
+            fluid_flow = Port('fluid-flow:{}'.format(i))
+            vortex.add_port(fluid_flow)
+
+            # Network connectivity (connect ports)
+            external_flow.connect(fluid_flow)
+            visualization.connect(plot)
+
+            cortix.add_module(droplet)
+
+        cortix.add_module(vortex)
+        cortix.add_module(data_plot)
+
+    # Network for a multiple plot case
+    if not use_single_plot:
+
+        # Vortex module (single).
+        vortex = Vortex()
+        #cortix.add_module(vortex)
+        vortex.show_time = (True,100)
+        vortex.end_time = end_time
+        vortex.time_step = time_step
+        vortex.plot_velocity()
+
+        for i in range(n_droplets):
+
+            # Droplet modules.
+            droplet = Droplet()
+            #cortix.add_module(droplet)
+            droplet.end_time = end_time
+            droplet.time_step = time_step
+            droplet.bounce = False
+            droplet.slip = False
+            # Ports def.
+            external_flow = Port('external-flow')
+            droplet.add_port(external_flow)
+            visualization = Port('visualization')
+            droplet.add_port(visualization)
+
+            # DataPlot modules (multiple).
             data_plot = DataPlot()
             #cortix.add_module(data_plot)
             data_plot.title = 'Droplet Trajectory '+str(i)
             data_plot.dpi = 300
-        # Ports def.
-        plot = Port('viz-data:{:05}'.format(i))
-        data_plot.add_port(plot)
+            # Ports def.
+            plot = Port('viz-data:{:05}'.format(i))
+            data_plot.add_port(plot)
 
-        # Vortex module.
-        # Ports def.
-        fluid_flow = Port('fluid-flow:{}'.format(i))
-        vortex.add_port(fluid_flow)
+            # Vortex module.
+            # Ports def.
+            fluid_flow = Port('fluid-flow:{}'.format(i))
+            vortex.add_port(fluid_flow)
 
-        # Network connectivity (connect ports)
-        external_flow.connect(fluid_flow)
-        visualization.connect(plot)
+            # Network connectivity (connect ports)
+            external_flow.connect(fluid_flow)
+            visualization.connect(plot)
 
-        cortix.add_module(droplet)
-
-        if not use_single_plot:
+            cortix.add_module(droplet)
             cortix.add_module(data_plot)
 
-    cortix.add_module(vortex)
+        cortix.add_module(vortex)
 
-    if use_single_plot:
-        cortix.add_module(data_plot)
-
-    cortix.draw_network("network.png")
+    cortix.draw_network('network.png')
 
     cortix.run()
