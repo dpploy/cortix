f18f263a230771e35ee26078599dd0b3e527fa40
Valmor F. de Almeida <dealmeidavf@gmail.com>
2019-07-31 03:11:08 -0400

    Updates on issue #197

--- cortix/tests/mpi_test_send_recv.py
+++ cortix/tests/mpi_test_send_recv.py
@@ -1,5 +1,4 @@
 from cortix.src.module import Module
-from cortix.src.port import Port
 
 from cortix.examples.dummy_module import DummyModule
 from cortix.examples.dummy_module import DummyModule2
@@ -8,22 +7,19 @@ from cortix.src.cortix_main import Cortix
 
 def mpi_send_recv():
 
-    # Create and connect P1 to P2
-    p1 = Port("test-port1")
-    p2 = Port("test-port2")
-    p1.connect(p2)
+    c = Cortix()
 
     m1 = DummyModule()
-    m1.add_port(p1)
+    c.add_module(m1)
+    p1 = m1.get_port('test-port1')
 
     m2 = DummyModule2()
-    m2.add_port(p2)
-
-    c = Cortix()
-    c.add_module(m1)
     c.add_module(m2)
-    c.run()
+    p2 = m2.get_port('test-port2')
 
+    p1.connect(p2)
+
+    c.run()
 
 if __name__ == "__main__":
     mpi_send_recv()
