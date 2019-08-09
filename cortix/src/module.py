c0a93fac40db8a71ed13e84746d16bb81ed32050
Taha Azzaoui <taha@azzaoui.org>
2019-08-09 14:39:38 -0400

    Documented module.py

--- cortix/src/module.py
+++ cortix/src/module.py
@@ -6,44 +6,62 @@
 from cortix.src.port import Port
 
 class Module:
-    '''
-    The representation of a Cortix module.
+    '''Cortix module super class
+
+    This class provides facilities for creating modules within the Cortix network.
+    Cortix will map one object of this class to either a Multiprocessing or MPI
+    process depending on the user's configuration.
+
+
+    Note
+    ----
     This class is to be inherited by every Cortix module.
-    It provides facilities for creating modules within the Cortix network.
-    In addition Cortix will map one object of this class to a Python
-    multiprocessing child process, or one executable object of this class to
-    one MPI process.
+    In order to execute, modules *must* override the `run` method, which will be
+    executed during the simulation
+
+    Attributes
+    ----------
+    name: str
+        A name given to the instance. Default is `None`.
+    port_names_expected: list(str), None
+        A list of names of ports expected in the module. This will be compared
+        to port names during runtime to check against the intended use of the
+        module.
+    state: any
+        Any `pickle-able` data structure to be passed in a `multiprocessing.Queue`
+        to the parent process or to be gathered in the root MPI process.
+        Default is `None`.
+    use_mpi: bool
+        True for MPI, false for Multiprocessing
+    ports: list(Port)
+        A list of ports contained by the module
+
     '''
 
     def __init__(self):
-        '''
-        Attributes
-        ----------
-        name: str
-            A name given to the instance. Default is `None`.
-        port_names_expected: list(str) or None
-            A list of names of ports expected in the module. This will be compared
-            to port names during runtime to check against the intended use of the
-            module.
-        state: any
-            Any `pickle-able` data structure to be passed in a `multiprocessing.Queue`
-            to the parent process or to be gathered in the root MPI process.
-            Default is `None`.
-        '''
+        '''Module super class constructor
 
+        This must be called in the constructor of every Cortix module like so:
+
+        >> super()__init__()
+
+       '''
         self.name = None
-
-        self.port_names_expected = None  # list of expected port names
-
-        self.state = None  # state data passed to parent process if any
-
+        self.port_names_expected = None
+        self.state = None
         self.use_mpi = False
-
         self.ports = []
 
     def send(self, data, port):
-        '''
-        Send data through a given port.
+        '''Send data through a given port.
+
+        Parameters
+        ----------
+        data: any
+            The data being sent out - must be pickleable
+        port: Port, str
+            A Port object to send the data through, or its string name
+
         '''
 
         if isinstance(port, str):
@@ -56,8 +74,22 @@ class Module:
         port.send(data)
 
     def recv(self, port):
-        '''
-        Receive data from a given port
+        '''Receive data from a given port
+
+        Warning
+        -------
+        This function will block until data is available
+
+        Parameters
+        ----------
+        port: Port, str
+            A Port object to send the data through, or its string name
+
+        Returns
+        -------
+        data: any
+            The data received through the port
+
         '''
 
         if isinstance(port, str):
@@ -70,8 +102,18 @@ class Module:
         return port.recv()
 
     def get_port(self, name):
-        '''
-        Get port by name; if it does not exist, create one.
+        '''Get port by name; if it does not exist, create one.
+
+        Parameters
+        ----------
+        name: str
+            The name of the port to get
+
+        Returns
+        -------
+        port: Port
+            The port object with the corresponding name
+
         '''
 
         assert isinstance(name, str), 'port name must be of type str'
@@ -92,14 +134,11 @@ class Module:
         return port
 
     def connect(self, port_name_or_module, to_other_port=None):
-        '''
-        A simpler interface (as compared to direct `port` connection) to create
-        module connectivity. Connect the module `port` (or `module`) to a given
-        `to_other_port` port.
+        '''Connect two modules using ports corresponding to their name
 
         Parameters
         ----------
-        port_name_or_module: str or Module
+        port_name_or_module: str, Module
             Either a `port` name or a `Module` can be given. In the latter case
             the `name` attribute of the module will be used to get the `port`
             of the module passed. This port will be connected to the port of the
@@ -129,9 +168,9 @@ class Module:
             my_port = self.get_port(port_name)
             my_port.connect(to_other_port)
 
-    #def run(self, state_comm=None, idx_comm=None):
     def run(self, *args):
-        '''
+        '''Module run function
+
         Run method with an option to pass data back to the parent process when running
         in Python multiprocessing mode. If the user does not want to share data with
         the parent process, this function can be overriden with `run(self)`
@@ -155,6 +194,10 @@ class Module:
 
         at the bottom of the user defined run() function.
 
+        Warning
+        -------
+        This function must be overridden by all Cortix modules
+
         Parameters
         ----------
         comm_idx: int
@@ -162,8 +205,7 @@ class Module:
 
         comm_state: multiprocessing.Queue
             When using the Python `multiprocessing` library `state_comm` must have
-            the module's `self.state` in it. That is,
-            `state_comm.put((idx_comm,self.state))` must be the last command in the
-            method before `return`. In addition, self.state must be `pickle-able`.
+            the module's `self.state` in it. That is, `state_comm.put((idx_comm,self.state))`
+            must be the last command in the method before `return`. In addition, self.state must be `pickle-able`.
         '''
         raise NotImplementedError('Module must implement run()')
