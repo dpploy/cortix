06bbe87dd6475d6cd27ed09c64d2692a9dfe9cfd
Taha Azzaoui <taha@azzaoui.org>
2019-08-26 14:26:30 -0400

    Bug fix for saving

--- cortix/src/network.py
+++ cortix/src/network.py
@@ -8,8 +8,8 @@ import pickle
 import logging
 from multiprocessing import Process, Queue
 
-from cortix.src.module import Module
-from cortix.src.port import Port
+from cortix import Module
+from cortix import Port
 
 class Network:
     '''Cortix network.
@@ -252,12 +252,11 @@ class Network:
 
         # Reload saved modules
         old_mods = list()
-        for mod in self.modules:
-            for file_name in os.listdir(".ctx-saved"):
-                if file_name.endswith(".pkl"):
-                    file_name = os.path.join(".ctx-saved", file_name)
-                    with open(file_name, "rb") as f:
-                        old_mods.append(pickle.load(f))
+        for file_name in os.listdir(".ctx-saved"):
+            if file_name.endswith(".pkl"):
+                file_name = os.path.join(".ctx-saved", file_name)
+                with open(file_name, "rb") as f:
+                    old_mods.append(pickle.load(f))
         self.modules = old_mods
 
     def draw(self, graph_attr=None, node_attr=None, engine=None):
