8d6ce2fdbac7942583d59ca46f06c03948aae736
Seamus Gallagher <seamusdgallagher@gmail.com>
2019-10-04 14:14:28 -0400

    Update __init__.py

--- cortix/examples/__init__.py
+++ cortix/examples/__init__.py
@@ -9,11 +9,11 @@ from cortix.examples.ideal_gas.particle_handler import Particle_Handler
 from cortix.examples.ideal_gas.run_particle import Run_Particle
 
 
-from cortix.examples.city_justice.prison import Prison
-from cortix.examples.city_justice.parole import Parole
-from cortix.examples.city_justice.adjudication import Adjudication
-from cortix.examples.city_justice.jail import Jail
-from cortix.examples.city_justice.arrested import Arrested
-from cortix.examples.city_justice.probation import Probation
-from cortix.examples.city_justice.community import Community
+##from cortix.examples.city_justice.prison import Prison
+##from cortix.examples.city_justice.parole import Parole
+##from cortix.examples.city_justice.adjudication import Adjudication
+##from cortix.examples.city_justice.jail import Jail
+##from cortix.examples.city_justice.arrested import Arrested
+##from cortix.examples.city_justice.probation import Probation
+##from cortix.examples.city_justice.community import Community
 
