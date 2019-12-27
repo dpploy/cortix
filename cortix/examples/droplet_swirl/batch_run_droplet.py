b279d14e4b28f43ccbc14210509d9c907d5e38a0
Taha Azzaoui <taha@azzaoui.org>
2019-12-27 12:22:43 -0500

    Time droplet runs and output elapsed time

--- cortix/examples/droplet_swirl/batch_run_droplet.py
+++ cortix/examples/droplet_swirl/batch_run_droplet.py
@@ -7,10 +7,12 @@ def main():
     num_drops = 2
     while num_drops < 2**10:
         num_procs = 2 * num_drops + 1
-        cmd = "mpirun -np {} run_droplet_swirl.py {}".format(num_procs, num_drops)
+        cmd = "mpirun -np {} run_droplet_swirl.py {} > /dev/null 2>&1".format(num_procs, num_drops)
         before = time.time()
+        print("Running with {} droplets and {} processes...".format(num_drops, num_procs))
         os.system(cmd)
         elapsed_time = time.time() - before
+        print("Elapsed time: {}".format(elapsed_time))
         num_drops *= 2
 
 if __name__ == "__main__":
