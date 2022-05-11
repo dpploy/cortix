8a1cd9daa29f139cc343bb15a9c487f9d7eec046
Valmor F. de Almeida <dealmeidavf@gmail.com>
2022-05-11 15:35:35 -0400

    version bump; parameter optimization support for reaction mechanisms

--- cortix/support/chemeng/reaction_mechanism.py
+++ cortix/support/chemeng/reaction_mechanism.py
@@ -5,11 +5,15 @@
 '''
 Suupport class for working with chemical reactions.
 '''
+import math
 import numpy as np
 
 from cortix.support.species import Species
+
 class ReactionMechanism:
     """Chemical reaction mechanism.
+    Quantites and services: stoichiometric matrix, mass conservation, reaction rate density vector,
+    species production rate density vector.
 
     Attributes
     ----------
@@ -24,17 +28,20 @@ class ReactionMechanism:
         List of species names.
 
     species: list(Species)
-        List of Species.
+        List of Species corresponding to the `species_names` list.
 
-    data: dict
-        Dictionary (key, value) of data given in the reaction mechanism.
-        Special keys: `alpha`, `beta` contain a dictionary of the empirical power-law exponents
-        of reaction rates for reactants and products respectively.
-        E.g.: self.data['alpha'] = {'e^-': 1e7, 'NO3': 15}
+    data: list(dict)
+        List (one element per reaction) of dicitionaries (key, value) of data given in the reaction
+        mechanism.  Special keys: `alpha`, `beta` contain a dictionary of the empirical power-law
+        exponents of reaction rates for reactants and products respectively; `k_f`, `k_b` contain the
+        reaction rate constants  per reaction.
+        E.g.: self.data[idx]['alpha'] = {'e^-': 1e7, 'NO3': 15}
+              self.data[idx]['k_f'] = 1e3
 
     stoic_mtrx: numpy.ndarray
         Stoichiometric matrix; 2D `numpy` array.
     """
+
     def __init__(self, header='no-header', file_name=None, mechanism=None, order_species=True):
         """Module class constructor.
 
@@ -141,6 +148,7 @@ class ReactionMechanism:
                     name = datum.split('=')[0].strip()
                     val_str = datum.split('=')[1].strip()
 
+                    # alpha and beta names are reserved for the exponents of the reaction rate law
                     # alpha and beta cases; convert tuple of integers into dictionary
                     if name == 'alpha' or name == 'beta':
                         assert '(' in val_str and ')' in val_str
@@ -161,23 +169,23 @@ class ReactionMechanism:
 
         species_tmp = list()  # temporary list for species
 
-        for r in self.reactions:
+        for idx, rxn in enumerate(self.reactions):
 
             # the order of the following test matters; test reversible reaction first
-            tmp = r.split('<=>')
+            tmp = rxn.split('<=>')
             n_terms = len(tmp)
             assert n_terms == 1 or n_terms == 2
 
             if n_terms == 1: # if no previous split
-                tmp = r.split('<->')
+                tmp = rxn.split('<->')
                 n_terms = len(tmp)
                 assert n_terms == 1 or n_terms == 2
                 if n_terms == 1: # if no previous split
-                    tmp = r.split('->')
+                    tmp = rxn.split('->')
                     n_terms = len(tmp)
                     assert n_terms == 1 or n_terms == 2
                     if n_terms == 1: # if no previous split
-                        tmp = r.split('<-')
+                        tmp = rxn.split('<-')
                         n_terms = len(tmp)
                         assert n_terms == 1 or n_terms == 2
 
@@ -267,7 +275,7 @@ class ReactionMechanism:
 
                 if 'alpha' in self.data[i_row].keys():
                     species_name = self.species_names[j_col]
-                    assert len(self.data[i_row]['alpha']) == len(left_terms), 'incorrect length of alpha.'
+                    assert len(self.data[i_row]['alpha']) == len(left_terms), 'Incorrect length of alpha.'
                     # replace species index with name
                     self.data[i_row]['alpha'][species_name] = self.data[i_row]['alpha'].pop(left_terms.index(t))
 
@@ -291,11 +299,12 @@ class ReactionMechanism:
 
                 if 'beta' in self.data[i_row].keys():
                     species_name = self.species_names[j_col]
-                    assert len(self.data[i_row]['beta']) == len(right_terms), 'incorrect length of beta.'
+                    assert len(self.data[i_row]['beta']) == len(right_terms), 'Incorrect length of beta.'
                     # replace species index with name
                     self.data[i_row]['beta'][species_name] = self.data[i_row]['beta'].pop(right_terms.index(t))
 
         self.stoic_mtrx = s_mtrx
+
     def mass_balance_residuals(self):
         """Reaction mass balance residual vector.
 
@@ -316,6 +325,7 @@ class ReactionMechanism:
         mb_residual_vec = self.stoic_mtrx @ m_vec
 
         return mb_residual_vec
+
     def max_mass_balance_residual(self):
         """Compute the maximum magnitude reaction mass balance residual.
 
@@ -329,6 +339,7 @@ class ReactionMechanism:
         mb_residual_vec = self.mass_balance_residuals()
 
         return np.max(np.abs(mb_residual_vec))
+
     def is_mass_conserved(self, tol=1e-10):
         """Check mass conservation if species have a molar mass value.
 
@@ -342,7 +353,8 @@ class ReactionMechanism:
         residual = self.max_mass_balance_residual()
 
         return True if residual < tol else False
-    def rank_analysis(self, tol=1e-8):
+
+    def rank_analysis(self, verbose=False, tol=1e-8):
         """Compute the rank of the stoichiometric matrix.
 
         This will establish rank deficiency.
@@ -350,14 +362,13 @@ class ReactionMechanism:
         Parameters
         ----------
 
+        verbose: bool
         tol: float
 
         Returns
         -------
 
         rank: int
-
-        print out
         """
 
         #assert self.is_mass_conserved(tol), 'fatal: mass conservation failed'
@@ -366,15 +377,17 @@ class ReactionMechanism:
 
         assert s_rank <= min(self.stoic_mtrx.shape)
 
-        print('*********************')
-        print('# reactions = ', len(self.reactions))
-        print('# species   = ', len(self.species))
-        print('rank of S = ', s_rank)
-        if s_rank == min(self.stoic_mtrx.shape):
-            print('S is full rank.')
-        else:
-            print('S is rank deficient.')
-        print('*********************')
+        if verbose:
+            print('*********************')
+            print('# reactions = ', len(self.reactions))
+            print('# species   = ', len(self.species))
+            print('rank of S = ', s_rank)
+
+            if s_rank == min(self.stoic_mtrx.shape):
+                print('S is full rank.')
+            else:
+                print('S is rank deficient.')
+            print('*********************')
 
         if s_rank == self.stoic_mtrx.shape[1]:
             print('***********************')
@@ -383,6 +396,180 @@ class ReactionMechanism:
             print('***********************')
 
         return s_rank
+
+    def r_vec(self, spc_molar_cc_vec, kf_vec=None, kb_vec=None, alpha_lst=None, beta_lst=None):
+        """Compute the reaction rate density vector.
+
+        Parameters
+        ----------
+        spc_molar_cc_vec: numpy.ndarray
+        Ordered (same as the reaction mechanism) molar concentration vector.
+
+        kf_vec: numpy.ndarray
+        Vector of forward reaction rate constants per reaction. If not provided, it will be assembled
+        internally.
+
+        kb_vec: numpy.ndarray
+        Vector of backward reaction rate constants per reaction. If not provided, it will be assembled
+        internally.
+
+        alpha_lst: list(numpy.ndarray)
+        List of alpha power-law exponents as a vector ordered in the order of appearance of reactants
+        in the corresponding reaction. If not provided, it will be assembled internally.
+
+        beta_lst: list(numpy.ndarray)
+        List of beta power-law exponents as a vector ordered in the order of appearance of products
+        in the corresponding reaction. If not provided, it will be assembled internally.
+        """
+        assert isinstance(spc_molar_cc_vec, np.ndarray)
+        assert spc_molar_cc_vec.size == len(self.species)
+
+        if kf_vec is not None:
+            assert isinstance(kf_vec, np.ndarray)
+            assert kf_vec.size == len(self.reactions)
+
+        if kb_vec is not None:
+            assert isinstance(kb_vec, np.ndarray)
+            assert kb_vec.size == len(self.reactions)
+
+        if alpha_lst is not None:
+            assert isinstance(alpha_lst, list)
+            assert len(alpha_lst) == len(self.reactions)
+            assert isinstance(alpha_lst[0], np.ndarray)
+
+        if beta_lst is not None:
+            assert isinstance(beta_lst, list)
+            assert len(beta_lst) == len(self.reactions)
+            assert isinstance(beta_lst[0], np.ndarray)
+
+        # Compute the reaction rate density vector
+        r_vec = np.zeros(len(self.reactions), dtype=np.float64)
+
+        for (idx, rxn_data) in enumerate(self.data):
+
+            (reactants_ids, ) = np.where(self.stoic_mtrx[idx, :] < 0)
+
+            reactants_molar_cc = spc_molar_cc_vec[reactants_ids] # must be oredered as in rxn_mech
+
+            if kf_vec is not None:
+                kf_i = kf_vec[idx]
+            else:
+                kf_i = rxn_data['k_f']
+
+            if alpha_lst is not None:
+                alpha_vec = alpha_lst[idx]
+            else:
+                alpha_lst_local = list()
+                for jdx in reactants_ids:
+                    alpha_lst_local.append(rxn_data['alpha'][self.species_names[jdx]])
+                alpha_vec = np.array(alpha_lst_local)
+
+            r_vec[idx] = kf_i * np.prod(reactants_molar_cc**alpha_vec)
+
+        for (idx, rxn_data) in enumerate(self.data):
+
+            (products_ids, ) = np.where(self.stoic_mtrx[idx, :] > 0)
+
+            products_molar_cc = spc_molar_cc_vec[products_ids] # must be oredered as in rxn_mech
+
+            if kb_vec is not None:
+                kb_i = kb_vec[idx]
+            else:
+                kb_i = rxn_data['k_b']
+
+            if beta_lst is not None:
+               beta_vec = beta_lst[idx]
+            else:
+                beta_lst_local = list()
+                for jdx in products_ids:
+                    beta_lst_local.append(rxn_data['beta'][self.species_names[jdx]])
+                beta_vec = np.array(beta_lst_local)
+
+            r_vec[idx] -= kb_i * np.prod(products_molar_cc**beta_vec)
+
+        return r_vec
+
+    def g_vec(self, spc_molar_cc_vec, kf_vec=None, kb_vec=None, alpha_lst=None, beta_lst=None):
+        """Compute the species production rate density vector.
+
+        Parameters:
+        -----------
+        """
+
+        g_vec = self.stoic_mtrx.transpose() @ self.r_vec(spc_molar_cc_vec, kf_vec, kb_vec, alpha_lst, beta_lst)
+
+        return g_vec
+
+    def drdtheta_mtrx(self, spc_molar_cc_vec, kf_vec, kb_vec, alpha_lst, beta_lst):
+        """Partial derivative of the reaction rate law wrt parameters.
+
+        The parameters in the derivative are ordered as: k_fs, k_bs, alphas, betas.
+        """
+
+        # Partial r_vec partial kf matrix
+        dr_dk_f = np.zeros((len(self.reactions),len(self.reactions)), dtype=np.float64)
+        # Partial r_vec partial kb matrix
+        dr_dk_b = np.zeros((len(self.reactions),len(self.reactions)), dtype=np.float64)
+
+        # Partial r_vec partial alpha
+        n_alphas = 0
+        for alpha_vec in alpha_lst:
+            n_alphas += alpha_vec.size
+
+        dr_dalpha = np.zeros((len(self.reactions),n_alphas), dtype=np.float64)
+
+        # Partial r_vec partial beta
+        n_betas = 0
+        for beta_vec in beta_lst:
+            n_betas += beta_vec.size
+
+        dr_dbeta = np.zeros((len(self.reactions),n_betas), dtype=np.float64)
+
+        for (idx, rxn_data) in enumerate(self.data):
+
+            (reactants_ids, ) = np.where(self.stoic_mtrx[idx, :] < 0)
+
+            reactants_molar_cc = spc_molar_cc_vec[reactants_ids]
+
+            spc_cc_power_prod = np.prod(reactants_molar_cc**alpha_lst[idx])
+
+            dr_dk_f[idx, idx] = spc_cc_power_prod
+
+            rf_i = kf_vec[idx] * spc_cc_power_prod
+
+            min_c_j = reactants_molar_cc.min()
+            if min_c_j <= 1e-8:
+                #print('min_c_j=',min_c_j)
+                (jdx, ) = np.where(reactants_molar_cc == min_c_j)
+                reactants_molar_cc[jdx] = 1.0
+
+            for (jdx, c_j) in enumerate(reactants_molar_cc):
+                dr_dalpha[idx, jdx] = math.log(c_j) * rf_i
+
+        for idx, rxn_data in enumerate(self.data):
+
+            (products_ids, ) = np.where(self.stoic_mtrx[idx, :] > 0)
+
+            products_molar_cc = spc_molar_cc_vec[products_ids]
+
+            spc_cc_power_prod = np.prod(products_molar_cc**beta_lst[idx])
+
+            dr_dk_b[idx, idx] = - spc_cc_power_prod
+
+            rb_i = kb_vec[idx] * spc_cc_power_prod
+
+            min_c_j = products_molar_cc.min()
+            if min_c_j <= 1e-8:
+                #print('min_c_j=',min_c_j)
+                (jdx, ) = np.where(products_molar_cc == min_c_j)
+                products_molar_cc[jdx] = 1.0
+
+            for (jdx, c_j) in enumerate(products_molar_cc):
+                dr_dbeta[idx, jdx] = - math.log(c_j) * rb_i
+
+        drdtheta_mtrx = np.hstack([dr_dk_f, dr_dk_b, dr_dalpha, dr_dbeta])
+
+        return drdtheta_mtrx
 
     def __str__(self):
         s = '\n\t **ReactionMechanism()**:' + \
